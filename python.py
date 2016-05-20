a9c456c68d5710f02a1a506bb5c4a9c8e890b875
Colin Osborn <colin.osborn@gmail.com>
2016-05-20 15:12:01 -0600

    python

---
+++ python.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self, num_passengers):
+        self.num_passengers = num_passengers
+
+    def print_num_passengers(self):
+        print self.num_passengers
+
+
+car = Car(5)
+car.print_num_passengers
+
+def 
